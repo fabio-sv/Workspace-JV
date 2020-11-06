e570b0ec54a2f1d984278b2cd4688a71d3baac1a
Fabio <fabio_dev@outlook.com>
2020-11-05 21:04:38 -0300

    update

---
+++ EstruturaDeDados/src/ClasseMath/MathMax.java
@@ -0,0 +1,14 @@
+package ClasseMath;
+
+public class MathMax {
+    public static void main(String[] args) {
+
+        int x = 10;
+        int y = 20;
+
+        // compara qual variável é a maior
+        System.out.println(Math.max(x, y));
+
+        // saída 20
+    }
+}
