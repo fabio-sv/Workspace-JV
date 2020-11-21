f5aa5d0a292070b8cd6c4267765e062eed0c4482
Fabio <fabio_dev@outlook.com>
2020-11-20 21:59:06 -0300

    update

---
+++ JavaWeb/src/modulo06/PrimeiraClasse.java
@@ -0,0 +1,9 @@
+package modulo06;
+
+public class PrimeiraClasse {
+    public static void main(String[] args) {
+
+        System.out.println("Meu primeiro programa Java");
+        System.out.println("Java é orientado a objeto");
+    }
+}
