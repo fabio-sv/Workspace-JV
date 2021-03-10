107a70586426f47c2d1177af3c1b8a0fbaec7f3a
Fabio <fabio_dev@outlook.com>
2021-03-10 18:38:47 -0300

    Update

---
+++ EstruturaDeDados/src/LambdaExpressoes/Foreach.java
@@ -0,0 +1,18 @@
+package LambdaExpressoes;
+
+import java.util.ArrayList;
+
+public class Foreach {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> vetor = new ArrayList<Integer>();
+        
+        vetor.add(10);
+        vetor.add(20);
+        vetor.add(30);
+
+        //Expressão Lambda
+        vetor.forEach((i) -> System.out.println(i));
+      
+    }
+}
